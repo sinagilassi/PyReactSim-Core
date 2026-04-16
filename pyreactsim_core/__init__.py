e94267918671ce185ad7175bb8dd7530da535610
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-04-15 21:40:53 -0400

    Add configuration module with package metadata

---
+++ pyreactsim_core/__init__.py
@@ -0,0 +1,19 @@
+# config
+from .configs.info import (
+    __version__,
+    __author__,
+    __package_name__,
+    __description__,
+    __email__,
+    __license__,
+)
+
+__all__ = [
+    # config
+    "__version__",
+    "__author__",
+    "__package_name__",
+    "__description__",
+    "__email__",
+    "__license__",
+]
---
+++ pyreactsim_core/configs/info.py
@@ -0,0 +1,12 @@
+# version
+__version__ = "0.1.0"
+# author
+__author__ = "Sina Gilassi"
+# email
+__email__ = "sina.gilassi@gmail.com"
+# license
+__license__ = "Apache-2.0"
+# package name
+__package_name__ = "PyReactSim-Core"
+# package description
+__description__ = "PyReactSim-Core is the core foundation of the PyReactSim ecosystem, offering shared data structures and algorithms for creating reaction rates."
